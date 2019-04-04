7995b90804472795552b35268af64047eab89d0d
Moses Park <mosespark@outlook.com>
2019-04-04 13:01:24 -0500

    Convert to MVP design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Moses Park on 4/4/19.
+//  Copyright © 2019 Moses Park. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
